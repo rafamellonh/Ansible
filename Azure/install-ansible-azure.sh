2c13e4dc83634a7b85a5ead61e526888e60dd539
rafael Mello <rmellonh@gmail.com>
2025-09-18 16:27:41 -0400

    Ansible

--- Azure/install-ansible-azure.sh
+++ Azure/install-ansible-azure.sh
@@ -11,5 +11,5 @@ sudo apt install ansible
 # Install Ansible az collection for interacting with Azure. (optional)
 ansible-galaxy collection install azure.azcollection --force 
 
-# Install Ansible modules for Azure (optional)
-sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
+# Install Ansible modules for Azure (optional) Usar Sem sudo para nao ter o erro PEP 668.
+pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
